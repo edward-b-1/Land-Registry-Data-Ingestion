eb2a243031a5e3cfcd6f2f2206a5ee516c0c1799
Bird <example@example.com>
2024-08-25 17:27:32 +0100

    fixed archiver so that it uses the database row id, and other fixes

--- Land-Registry-Download/land_registry_pp_monthly_update_archiver.py
+++ Land-Registry-Download/land_registry_pp_monthly_update_archiver.py
@@ -30,6 +30,7 @@ from lib_land_registry_data.lib_dto import PPMonthlyUpdateDataDecisionNotificati
 from lib_land_registry_data.lib_dto import PPMonthlyUpdateArchiveNotificationDTO
 
 from lib_land_registry_data.lib_db import PPMonthlyUpdateDownloadFileLog
+from lib_land_registry_data.lib_db import PPMonthlyUpdateArchiveFileLog
 
 from lib_land_registry_data.lib_env import EnvironmentVariables
 
@@ -109,7 +110,7 @@ def kafka_event_loop(
 
     logger.info(f'consumer subscribing to topic {TOPIC_NAME_PP_MONTHLY_UPDATE_DATA_DECISION_NOTIFICATION}')
     consumer.subscribe([TOPIC_NAME_PP_MONTHLY_UPDATE_DATA_DECISION_NOTIFICATION])
-    consumer_poll_timeout = 10.0
+    consumer_poll_timeout = 5.0
     logger.info(f'consumer poll timeout: {consumer_poll_timeout}')
 
     global exit_flag
@@ -142,6 +143,8 @@ def kafka_event_loop(
                 if notification_type == NOTIFICATION_TYPE_PP_MONTHLY_UPDATE_DATA_DECISION_COMPLETE:
                     logger.info(f'run archive process')
 
+                    pp_monthly_update_file_log_id = dto.pp_monthly_update_file_log_id
+
                     # new notification documents just trigger the garbage collection
                     # process for all old files
                     archive(
@@ -149,6 +152,7 @@ def kafka_event_loop(
                         postgres_engine=postgres_engine,
                         boto3_session=boto3_session,
                         minio_url=minio_url,
+                        pp_monthly_update_file_log_id=pp_monthly_update_file_log_id,
                     )
 
                 else:
@@ -167,76 +171,98 @@ def archive(
     postgres_engine: Engine,
     boto3_session,
     minio_url: str,
+    pp_monthly_update_file_log_id: int,
 ) -> None:
 
     with Session(postgres_engine) as session:
 
-        rows = (
+        row = (
             session
             .query(PPMonthlyUpdateDownloadFileLog)
-            .filter(PPMonthlyUpdateDownloadFileLog.gc_action_taken.is_(None))
-            .all()
+            .filter_by(pp_monthly_update_file_log_id=pp_monthly_update_file_log_id)
+            .one()
         )
-        logger.info(f'querying database table PPMonthlyUpdateDownloadFileLog for rows with process_decision=\'archive\'')
-        logger.info(f'{len(rows)} match database query')
+        logger.info(f'querying database table PPMonthlyUpdateDownloadFileLog for rows with pp_monthly_update_file_log_id=\'{pp_monthly_update_file_log_id}\'')
 
-        for row in rows:
-            data_decision = row.data_decision
+        data_decision = row.data_decision
 
-            if data_decision == 'archive':
-                logger.info(f'row with data_decision={data_decision}, run garbage collection')
+        if data_decision == 'archive':
+            logger.info(f'row with data_decision={data_decision}, run archiver')
 
-                bucket = row.s3_tmp_bucket
-                bucket_archive = 'land-registry-data-archive'
-                object_key = row.s3_tmp_object_key
-                logger.info(f'archive file from s3 in bucket {bucket} with object key {object_key} to bucket {bucket_archive}')
+            bucket = row.s3_tmp_bucket
+            bucket_archive = 'land-registry-data-archive'
+            object_key = row.s3_tmp_object_key
+            logger.info(f'archive file from s3 in bucket {bucket} with object key {object_key} to bucket {bucket_archive}')
 
-                boto3_client = (
-                    boto3_session.client(
-                        's3',
-                        endpoint_url=minio_url,
-                        config=botocore.Config(signature_version='s3v4'),
-                    )
+            boto3_client = (
+                boto3_session.client(
+                    's3',
+                    endpoint_url=minio_url,
+                    config=botocore.config.Config(signature_version='s3v4'),
                 )
-
-                s3_copy_start_datetime = datetime.now(timezone.utc)
-
-                # hope this action doesn't take longer than ~ 30 seconds, or Kafka will blow up
-                boto3_client.copy(
-                    CopySource={
-                        'Bucket': bucket,
-                        'Key': object_key,
-                    },
-                    Bucket=bucket_archive,
-                    Key=object_key,
-                )
-
-                boto3_client.delete_object(Bucket=bucket, Key=object_key)
-
-                s3_copy_complete_datetime = datetime.now(timezone.utc)
-                s3_copy_duration = s3_copy_complete_datetime - s3_copy_start_datetime
-
-                row.s3_archive_action_taken = 'archive'
-                row.s3_archive_datetime = datetime.now(timezone.utc)
-                row.s3_copy_start_datetime = s3_copy_start_datetime
-                row.s3_copy_duration = s3_copy_duration
-                session.commit()
-
-            elif data_decision == 'garbage_collect':
-                logger.info(f'row with data_decision={data_decision}, ignore')
-                row.s3_archive_action_taken = 'ignore'
-                row.s3_archive_datetime = datetime.now(timezone.utc)
-                session.commit()
-
-            else:
-                raise RuntimeError(f'invalid data_decision {data_decision}')
-
-            pp_monthly_update_file_log_id = row.pp_monthly_update_file_log_id
-
-            notify(
-                producer=producer,
-                pp_monthly_update_file_log_id=pp_monthly_update_file_log_id,
             )
+
+            s3_copy_start_timestamp = datetime.now(timezone.utc)
+
+            # hope this action doesn't take longer than ~ 30 seconds, or Kafka will blow up
+            boto3_client.copy(
+                CopySource={
+                    'Bucket': bucket,
+                    'Key': object_key,
+                },
+                Bucket=bucket_archive,
+                Key=object_key,
+            )
+
+            boto3_client.delete_object(Bucket=bucket, Key=object_key)
+
+            s3_copy_complete_timestamp = datetime.now(timezone.utc)
+            s3_copy_duration = s3_copy_complete_timestamp - s3_copy_start_timestamp
+
+            row.s3_archive_action_taken = 'archive'
+            row.s3_archive_datetime = datetime.now(timezone.utc)
+            row.s3_archive_bucket = bucket_archive
+            row.s3_archive_object_key = object_key
+            row.s3_copy_start_timestamp = s3_copy_start_timestamp
+            row.s3_copy_duration = s3_copy_duration
+            session.commit()
+
+            download_start_timestamp = row.download_start_timestamp
+
+            data_timestamp = (
+                datetime(
+                    year=download_start_timestamp.year,
+                    month=download_start_timestamp.month,
+                    day=download_start_timestamp.day,
+                    tzinfo=timezone.utc,
+                )
+            )
+
+            row = PPMonthlyUpdateArchiveFileLog(
+                created_datetime=datetime.now(timezone.utc),
+                data_source='current',
+                data_timestamp=data_timestamp,
+                s3_bucket=bucket_archive,
+                s3_object_key=object_key,
+            )
+            session.add(row)
+            session.commit()
+
+        elif data_decision == 'garbage_collect':
+            logger.info(f'row with data_decision={data_decision}, ignore')
+            row.s3_archive_action_taken = 'ignore'
+            row.s3_archive_datetime = datetime.now(timezone.utc)
+            session.commit()
+
+        else:
+            raise RuntimeError(f'invalid data_decision {data_decision}')
+
+        pp_monthly_update_file_log_id = row.pp_monthly_update_file_log_id
+
+        notify(
+            producer=producer,
+            pp_monthly_update_file_log_id=pp_monthly_update_file_log_id,
+        )
 
 
 def notify(
@@ -278,5 +304,6 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, ctrl_c_signal_handler)
     signal.signal(signal.SIGTERM, sigterm_signal_handler)
     main()
+    logger.info(f'process exit')
 
 
